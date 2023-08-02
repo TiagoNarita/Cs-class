fb695430491a3f2b129c3f0556dd1bc9bd5e9a69
Tiago Narita <132394947+TiagoNarita@users.noreply.github.com>
2023-08-01 22:01:37 -0300

    Create ScreenSound.cs

---
+++ AluraClasses/ScreenSound.cs
@@ -0,0 +1,37 @@
+// Screen Sound 
+string mensagemBoasVindas = "\nBoas vindas ao Screen Sound";
+
+
+void ExibirMensagemBoasVindas()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+    Console.WriteLine(mensagemBoasVindas);
+}
+
+void ExibirOpçoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda ");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas ");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidanumerica = int.Parse(opcaoEscolhida);
+    if(opcaoEscolhidanumerica == 1)
+    {
+        Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+    }
+
+}
+
+ExibirMensagemBoasVindas();
+ExibirOpçoesDoMenu();
+
